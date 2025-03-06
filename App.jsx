5e157c8f0cd99cb41ec12fb2cabe2a8b2d78f13e
Marcelo Gamboa <mfgamboa85@gmail.com>
2025-03-06 20:14:41 -0300

    add project to github page

--- App.jsx
+++ App.jsx
@@ -2,10 +2,10 @@ import Header from "./Header"
 import Main from "./Main"
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <Main />
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
 }
